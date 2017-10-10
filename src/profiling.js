1f715bc575d047451d5eb90cf5c2c14f165f4e1e
Timo Tijhof <krinklemail@gmail.com>
2017-10-10 01:14:04 +0100

    Add new php-profiling page
    The new page is now the destination for the "Flame Graphs" link
    instead of linking to the /xenon/ directory directly.
    
    * Document the source of the logs and graphs.
    
    * Show a thumbnail of a flame graph.
      Optimised PNG file with ImageOptim (using Zopfli).
    
    * Link to the daily/hourly graphs.
    
    * Link to the daily/hourly logs.
    
    * Link to the 5 most recent flame graphs, by entry point.
      This last section is created using client-side JavaScript to
      derive the links from the current date. The links are only an
      enhancement and are placed next to the thumbnail in a way that
      doesn't cause a layout chance, not does it look broken when
      the links are missing.
    
    * Given we now have two pages, update the header include to add
      class="active" to the right tab depending on which page is
      generated.
    
    * Update uncss command to also consider the new page.
    
    * Fork page layout so that d3/metrics-graphics are not loaded
      on this new page.
    
    Change-Id: I34cd09c19974b6a008a6a2504af1e11de68a7eb9

---
+++ src/profiling.js
@@ -0,0 +1,70 @@
+/**
+ * Create recent date links for PHP Profiling page.
+ *
+ * Copyright 2017 Timo Tijhof <krinklemail@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+( function () {
+	var span, i, d, isoDate;
+
+	// Feature test
+	if ( !Date.prototype.toISOString ) {
+		return;
+	}
+
+	function createUrl( file ) {
+		return 'https://performance.wikimedia.org/xenon/svgs/daily/' + file;
+	}
+
+	function appendLinks( parent, date, endpoint, label ) {
+		var link;
+
+		link = document.createElement( 'a' );
+		link.href = createUrl( date + '.' + endpoint + '.svgz' );
+		link.textContent = endpoint;
+		link.title = 'Flame graph for ' + date + ' of ' + label;
+		parent.appendChild( link );
+
+		link = document.createElement( 'a' );
+		link.href = createUrl( date + '.' + endpoint + '.reversed.svgz' );
+		link.textContent = 'rev';
+		link.title = 'Reversed flame graph for ' + date + ' of ' + label;
+		parent.appendChild( document.createTextNode( ' (' ) );
+		parent.appendChild( link );
+		parent.appendChild( document.createTextNode( ')' ) );
+	}
+
+	span = document.getElementById( 'flamegraph-current' );
+	span.textContent = 'Recent flame graphs by endpoint: ';
+
+	i = 1;
+	while ( i <= 5 ) {
+		d = new Date();
+		d.setDate( d.getDate() - i );
+		isoDate = d.toISOString().split( 'T' )[ 0 ];
+
+		span.appendChild( document.createElement( 'br' ) );
+		span.appendChild( document.createTextNode( '• ' + isoDate + ': ' ) );
+		appendLinks( span, isoDate, 'all', 'all MediaWiki requests' );
+		span.appendChild( document.createTextNode( ' • ' ) );
+		appendLinks( span, isoDate, 'index', 'index.php requests' );
+		span.appendChild( document.createTextNode( ' • ' ) );
+		appendLinks( span, isoDate, 'api', 'api.php requests' );
+		span.appendChild( document.createTextNode( ' • ' ) );
+		appendLinks( span, isoDate, 'load', 'load.php requests' );
+
+		i++;
+	}
+
+}() );
