9a9dec2fe1fde5231e32beb384a8f8e26f976897
Timo Tijhof <krinkle@fastmail.com>
2022-06-29 20:10:19 -0700

    asreport: Improve docs, simplify description, cross-link blogpost
    Also add some missing paragraph elements, and convert some double
    line breaks to paragraphs for more consistent design and readability
    (double BR creates a slightly bigger gap than regular paragraphs).
    
    Bug: T311680
    Change-Id: Id9d9481112832390e1d4423fc7d5a261c3ba3825

--- src/_plugins/asreport.rb
+++ src/_plugins/asreport.rb
@@ -2,6 +2,8 @@
 module Jekyll
   class ASGenerator < Generator
     def generate(site)
+      # The command in package.json uses `curl -R` to set the file mtime
+      # to the Last-Modified header from analytics.wikimedia.org.
       site.config['AS_REPORT_TIME'] = File.mtime('src/_data/asreport.tsv')
     end
   end
